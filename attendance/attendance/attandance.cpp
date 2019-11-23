ecb89d5fe7c11cc68ee472671de80b3b5062a3d2
HafizHamza19 <HafizHamza0322@gmail.com>
2019-11-23 19:06:56 +0500

    Initial commit

---
+++ attendance/attendance/attandance.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+char main (void)
+{
+	char hamza;
+	cout <<"Enter You Roll No: ";
+	cin >>hamza;
+   if (hamza=6666)
+	{   
+		cout <<"You are hamza";
+   }
+   else 
+   {
+	   cout <<"you are not hamza"<<endl;
+   }
+   system("pause");
+   return 0;
+}
